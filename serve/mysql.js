ad29c0790bbb4a619d5b9119a8094883f09ee649
shijiean <1592366283@qq.com>
2022-02-21 12:55:01 +0800

    deviceChange-test

--- serve/mysql.js
+++ serve/mysql.js
@@ -1,25 +1,25 @@
-const mysql=require('mysql')
+const mysql = require('mysql')
 
-const query=(sql,params,callback)=>{
-    const conn=mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'root',
-        dateStrings:true,
-        database:'blog'
+const query = (sql, params, callback) => {
+    const conn = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456789',
+        dateStrings: true,
+        database: 'blog'
     })
-    conn.connect(err=>{
-        if(err)
-        throw err
-    })
-    conn.query(sql,params,(err,results)=>{
-        if(err)
-        throw err
-        callback(results)
-        conn.end(err=>{
-            if(err)
+    conn.connect(err => {
+        if (err)
             throw err
+    })
+    conn.query(sql, params, (err, results) => {
+        if (err)
+            throw err
+        callback(results)
+        conn.end(err => {
+            if (err)
+                throw err
         })
     })
 }
-module.exports=query
+module.exports = query
